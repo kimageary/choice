d7e951769fe4e3664f12a105b366a3b7bca1edcf
Kimberly Mageary <kimberlymageary@gmail.com>
2024-10-03 12:50:12 -0700

    Tested me addContact method and included ID functionality

--- PhoneBook.java
+++ PhoneBook.java
@@ -1,14 +1,21 @@
 import java.util.*;
 public class PhoneBook {
-    private static HashMap<String, Contact> phoneBook = new HashMap<String, Contact>();
+    private static HashMap<Integer, Contact> phoneBook = new HashMap<Integer, Contact>();
+    private static int ID = 0;
     public static void main(String[] args) {
         
+        addContact("Hayden", "123455", "kasdjk@mail.comll");
+        addContact("Kim", "524324", "fsseas@mail.comll");
+        addContact("Maddy", "432424", "ageaa@mail.comll");
+        addContact("Aurora", "63242", "garfsa@mail.comll");
         
+        System.out.println(phoneBook);
     }
 
     public static void addContact(String name, String phoneNumber, String email){
-        Contact contact = new Contact("Kim", "23923", "kasda");
-        phoneBook.put(contact.getName(), contact);
+        Contact contact = new Contact(name, phoneNumber, email);
+        phoneBook.put(ID, contact);
+        ID++;g
     }
 
 }
